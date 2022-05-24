6c98e38b3b93ce85ac9ab0aaff674d6d029344ec
strox812 <strox812@gmail.com>
2022-05-24 14:29:08 +0800

    3 дз

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.urls import path
 from django.contrib import admin
 from mainapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('', views.index, name='index'),
