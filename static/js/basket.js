bc903e38a00bae70bfa1ecbb67b378c29f504191
strox <strox812@gmail.com>
2022-06-06 05:44:11 +0800

    6 урок

---
+++ static/js/basket.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // $('.basket_list input[type=number]').click(function(){
+    $('.basket_list').on(types: 'click', selector: 'input[type=number]', data: function(){
+        let pk = event.target.name;
+        let quantity = event.target.value;
+        $.ajax(url:{
+            url: '/basket/edit/' + pk + '/' + quantity + '/',
+            success: function(data){
+                $('.basket_list').html(data.result);
+            }
+        });
+    });
+});
