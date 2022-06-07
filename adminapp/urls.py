2024baaf8683d2bcaeb70fa9c2965c0c94dd46eb
strox812 <strox812@gmail.com>
2022-06-07 15:10:14 +0800

    урок 8

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -8,8 +8,8 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('users/create/', adminapp.user_create, name='user_create'),
-    path('users/read/', adminapp.user_read, name='user_read'),
-    path('users/update/<pk>/', adminapp.user_update, name='user_update'),
+    path('users/read/', adminapp.UserListView.as_view(), name='user_read'),
+    path('users/update/<pk>/', adminapp.UserUpdateView.as_view(), name='user_update'),
     path('users/delete/<pk>/', adminapp.user_delete, name='user_delete'),
 
     path('categories/create/', adminapp.category_create, name='category_create'),
--- adminapp/views.py
+++ adminapp/views.py
@@ -5,34 +5,61 @@ from django.contrib.auth.decorators import user_passes_test
 from adminapp.forms import UserAdminEditForm, ProductEditForm
 from authapp.models import ShopUser
 from mainapp.models import Category, Product
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class AccessMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
     return None
 
-@user_passes_test(lambda u: u.is_superuser)
-def user_read(request):
-    context = {
-        'objects': ShopUser.objects.all().order_by('is_active', 'is_superuser')
-    }
-    return render(request, 'adminapp/user_list.html', context)
+# @user_passes_test(lambda u: u.is_superuser)
+# def user_read(request):
+#     context = {
+#         'objects': ShopUser.objects.all().order_by('is_active', 'is_superuser')
+#     }
+#     return render(request, 'adminapp/user_list.html', context)
 
-@user_passes_test(lambda u: u.is_superuser)
-def user_update(request, pk):
-    user_item = get_object_or_404(ShopUser, pk=pk)
-    if request.method == 'POST':
-        edit_form = UserAdminEditForm(request.POST, request.FILES, instance=user_item)
-        if edit_form.is_valid():
-            edit_form.save()
-            return HttpResponseRedirect(reverse('adminapp:user_update', args=[pk]))
-    else:
-        edit_form = UserAdminEditForm(instance=user_item)
 
-    context = {
-        'form': edit_form
+class UserListView(AccessMixin, ListView):
+    model = ShopUser
+    template_name = 'adminapp/user_list.html'
+    paginate_by = 2
+
+    extra_context = {
+        'title': 'Список пользователей'
     }
-    return render(request, 'adminapp/user_form.html', context)
+
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def user_update(request, pk):
+#     user_item = get_object_or_404(ShopUser, pk=pk)
+#     if request.method == 'POST':
+#         edit_form = UserAdminEditForm(request.POST, request.FILES, instance=user_item)
+#         if edit_form.is_valid():
+#             edit_form.save()
+#             return HttpResponseRedirect(reverse('adminapp:user_update', args=[pk]))
+#     else:
+#         edit_form = UserAdminEditForm(instance=user_item)
+#
+#     context = {
+#         'form': edit_form
+#     }
+#     return render(request, 'adminapp/user_form.html', context)
+
+class UserUpdateView(AccessMixin, UpdateView):
+    model = ShopUser
+    template_name = 'adminapp/user_form.html'
+    form_class = UserAdminEditForm
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
